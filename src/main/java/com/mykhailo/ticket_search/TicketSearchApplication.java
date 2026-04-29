4594594b6194383854c6dc42bf1fcd6c32ce16e0
Mykhailo Vinnichuk <mishavprogram@ukr.net>
2026-04-29 13:13:10 +0300

    style: optimize imports and reformat code

--- src/main/java/com/mykhailo/ticket_search/TicketSearchApplication.java
+++ src/main/java/com/mykhailo/ticket_search/TicketSearchApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketSearchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketSearchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketSearchApplication.class, args);
+    }
 
 }
