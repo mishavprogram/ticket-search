2ad6237f6434baea70247644225332769fa9c3d0
Mykhailo Vinnichuk <mishavprogram@ukr.net>
2026-04-30 21:40:17 +0300

    chore: disable sample data initializer

--- src/main/java/com/mykhailo/ticket_search/config/DataInitializer.java
+++ src/main/java/com/mykhailo/ticket_search/config/DataInitializer.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
-@Component
+//@Component
 public class DataInitializer implements CommandLineRunner {
 
     private final TicketJpaRepository ticketJpaRepository;
